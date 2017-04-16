6139ca5e8310d52423392f4930b125277c13d017
Miguel Dorta <dortamiguel@gmail.com>
2017-04-17 00:06:10 +0100

    Improved line counting

--- MinimapHider.py
+++ MinimapHider.py
@@ -5,7 +5,7 @@ class MinimapHider(sublime_plugin.ViewEventListener):
 
     def on_modified(self):
         region = sublime.Region(0, self.view.size())
-        count = len(self.view.split_by_newlines(region))
+        count = self.view.rowcol(self.view.size())[0] + 1
         self.view.window().set_minimap_visible(count > 100)
 
     on_new = on_activated = on_clone = on_modified
--- Readme.md
+++ Readme.md
@@ -1,3 +1,5 @@
 # Minimap Hider
 
 > Shows the minimap only when the file has more than 100 lines. Sublime Text 3.
+
+The code of this repository is a result of a conversation in [this thread](https://forum.sublimetext.com/t/toggle-the-minimap-only-with-large-files/27589) on the sublime text forum.
