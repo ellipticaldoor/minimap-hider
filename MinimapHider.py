be7dd8dd9a5fa27e41c33fb7685f41a976d48e91
Miguel Dorta <dortamiguel@gmail.com>
2017-04-17 12:46:36 +0100

    Improvements https://forum.sublimetext.com/t/toggle-the-minimap-only-with-large-files/27589/5

--- MinimapHider.py
+++ MinimapHider.py
@@ -1,11 +1,9 @@
 import sublime_plugin
-import sublime
 
-class MinimapHider(sublime_plugin.ViewEventListener):
+class MinimapHider(sublime_plugin.EventListener):
 
-    def on_modified(self):
-        region = sublime.Region(0, self.view.size())
-        count = self.view.rowcol(self.view.size())[0] + 1
-        self.view.window().set_minimap_visible(count > 100)
+    def on_modified(self, view):
+        count = view.rowcol(view.size())[0] + 1
+        view.window().set_minimap_visible(count > 100)
 
-    on_new = on_activated = on_clone = on_modified
+    on_activated = on_modified
