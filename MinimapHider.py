8bac742acae4f5df802ba9be21ec4f165d1e9030
Miguel Dorta <dortamiguel@gmail.com>
2017-04-17 16:21:16 +0100

    Fixed hiding minimap on super + shift + p

--- MinimapHider.py
+++ MinimapHider.py
@@ -1,9 +1,9 @@
 import sublime_plugin
 
-class MinimapHider(sublime_plugin.EventListener):
+class MinimapHider(sublime_plugin.ViewEventListener):
 
-    def on_modified(self, view):
-        count = view.rowcol(view.size())[0] + 1
-        view.window().set_minimap_visible(count > 100)
+	def on_modified(self):
+		count = self.view.rowcol(self.view.size())[0] + 1
+		self.view.window().set_minimap_visible(count > 100)
 
-    on_activated = on_modified
+	on_activated = on_modified
